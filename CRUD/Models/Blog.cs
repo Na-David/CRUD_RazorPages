288c7257708303f55e5d7e46955d9cc8d60c79cd
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-06-27 23:53:26 +1200

    Blog basic settings

---
+++ CRUD/Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CRUD.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
