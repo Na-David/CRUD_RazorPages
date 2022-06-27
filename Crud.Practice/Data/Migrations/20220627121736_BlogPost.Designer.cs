8cba3b49421dfc7506ed881f3ab2bfc2d2a78356
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-06-28 00:22:31 +1200

    Updated to Database

--- Crud.Practice/Data/Migrations/20220627121736_BlogPost.Designer.cs
+++ Crud.Practice/Data/Migrations/20220627121736_BlogPost.Designer.cs
@@ -4,14 +4,16 @@ using Crud.Practice.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Crud.Practice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220627121736_BlogPost")]
+    partial class BlogPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
